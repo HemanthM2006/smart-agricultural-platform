2e5c2c66908ccf26d9c9cc25b9854b664c2183f7
HemanthM2006 <hemimpana@gmail.com>
2026-01-08 21:58:56 +0530

    Fix GitHub Pages routing using HashRouter

--- smart-agricultural-platform/frontend/vite.config.js
+++ smart-agricultural-platform/frontend/vite.config.js
@@ -1,7 +1,13 @@
 // frontend/vite.config.js
+
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base: "/smart-agricultural-platform/",
+  build: {
+    outDir: "../docs",
+    emptyOutDir: true
+  }
 })
